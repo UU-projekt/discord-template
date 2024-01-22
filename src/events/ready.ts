511455ea97506436c826cf96164762c84001b524
ffamilyfriendly <me@familyfriendly.xyz>
2024-01-22 02:07:40 +0100

    📝 adds content to readme file 🙈 fixes git history to undo pushing /dist

--- src/events/ready.ts
+++
@@ -1,8 +0,0 @@
-import { ActivityType, Client } from "discord.js";
-import { logger } from "../index";
-
-export default function(bot: Client) {
-    logger.info(`Logged in as ${bot.user?.username}!`)
-
-    bot.user?.setPresence({ activities:[ { name: process.env.DISCORD_BOT_STATUS, type: ActivityType.Custom } ], status: "online" })
-}
